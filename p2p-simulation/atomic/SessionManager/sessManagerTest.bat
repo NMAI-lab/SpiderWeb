0d3e78a0a053e4c0e826cf9b9eae332930929fc0
Denis <djdionne@connect.carleton.ca>
2011-06-07 15:33:40 -0400

    We added the simulation code, documentation, graphgenerator and logProcessing
    Change-Id: I3feb623b7cd880d35ba40874ace8af20d1aad215

---
+++ p2p-simulation/atomic/SessionManager/sessManagerTest.bat
@@ -0,0 +1,5 @@
+cd\ 
+cd eclipse\workspace\p2p\atomic\SessionManager\
+simu.exe -m"SessionManager.MA" -o"SessionOUT.out" -l"SessionLOG.log" -t"50:00:00:000"
+rem 
+pause
---
+++ p2p-simulation/coupled/Network70/P2PTest.bat
@@ -0,0 +1,4 @@
+cd \eclipse\workspace\p2p\coupled\Network70\
+simu.exe -m"P2PNet_70.MA" -l"P2P70LOG.log" -w9-9 -t"20:00:00:000"
+rem
+ pause
